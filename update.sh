9a5af3c8e8a7840221e7e59b92837f62812a439f
Gozar <github@ryancollins.org>
2019-06-28 15:12:39 +0000

    One command to rule all updates

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Updating system software..."
+sudo -v > /dev/null
+SUDOACCESS=$?
+
+if [ ${SUDOACCESS} -eq 0 ]; then
+    [ -z "$(find -H /var/lib/apt/lists -maxdepth 0 -mtime -1)" ] && sudo apt -qq update
+    sudo apt -qq upgrade
+else
+    echo "No sudo access, skipping update."
+fi
+
+# Update dotfiles repo
+echo "Checking/updating dotfiles..."    
+git pull --quiet
+
+#update vim plugins
+for i in ~/.vim/bundle/*; do 
+    echo "Checking/updating ${i}..."    
+    git -C $i pull --quiet
+done
+
