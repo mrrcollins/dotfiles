f538f28be2d1dd2d2cfe54e40fee060be04af729
Ryan <ryan@ryancollins.org>
2023-10-13 21:33:22 -0400

    Streamline setup on macOS

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,21 @@ elif [[ "$ostype" =~ "Darwin" ]]; then
     brew update
     brew upgrade
 	brew install $apps
+    echo "Clone alfredprefs"
+    if [ ! -d ~/.config/alfredprefs ]; then
+       git clone --quiet git@github.com:mrrcollins/alfredprefs.git ~/.config/alfredprefs
+    else
+       echo "~/.config/alfredprefs already exists..."
+    fi
+    echo "Clone iterm2prefs"
+    if [ ! -d ~/.config/iterm2prefs ]; then
+       git clone --quiet git@github.com:mrrcollins/iterm2prefs.git ~/.config/iterm2prefs
+    else
+       echo "~/.config/iterm2prefs already exists..."
+    fi
+    cd ~/Downloads
+    rsync -avp venkman:~/.config/macOS/ .
+    cd
 elif [ ! $android ]; then
     apt update
 	apt install $apps
@@ -39,6 +54,14 @@ if [ ! -d ~/.vim ]; then
 else
     echo "~/.vim already exists..."
 fi
+
+echo "Clone Espanso"
+if [ ! -d ~/.config/espanso ]; then
+   git clone --quiet git@github.com:mrrcollins/espanso.git ~/.config/espanso
+else
+   echo "~/.config/espanso already exists..."
+fi
+
 
 cd ~
 
