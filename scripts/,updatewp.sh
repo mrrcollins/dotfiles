3a408092fbf59240451fcc1aae1fc422a0eee7f3
Ryan 'Goz' Collins <goz@gozgeek.com>
2024-02-04 13:53:34 -0500

    Git Sync Sun 04 Feb 2024 01:53:34 PM EST on venkman

--- scripts/,updatewp.sh
+++ scripts/,updatewp.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-wp="/usr/local/bin/wp"
+wp=$(command -v wp)
+[ -z ${wp} ] && { echo "wp-cli not found!"; exit; }
 
-echo "Which blog? (enter the alias only):"
-cat "${HOME}/.wp-cli/config.yml" | grep "^@"
-read -e blog
+echo "Making sure wp-cli is up to date..."
+sudo wp cli update
 
-echo "Working on @${blog}..."
+echo -e "\nList of blogs:"
+cat "${HOME}/.wp-cli/config.yml" | grep "^@" | sed 's/^@//; s/:$//'
+read -p "Which blog do you want to work with? " blog
+
+echo -e "\nGetting ${blog}'s name and address...'"
+blogname=$(${wp} @${blog} option get blogname)
+bloghome=$(${wp} @${blog} option get home)
+
+read -p "Do you want to see what updates are available? " yn
+if ! echo "${yn}" | grep -Eiq "^n(o)?$"; then
+    echo "Checking the core of ${blogname} (${bloghome})..."
+    ${wp} @${blog} core check-update
+    echo "Checking plugins..."
+    ${wp} @${blog} plugin status | grep "^ U"
+    echo "Checking themes..."
+    ${wp} @${blog} theme status | grep "^ U"
+fi
+
+read -p "Update ${blogname} (${bloghome})? " yn
+[ "${yn}" = "n" ] && { echo "Aborting..."; exit; }
+
 ${wp} @${blog} core update
 ${wp} @${blog} plugin update --all
 ${wp} @${blog} theme update --all
