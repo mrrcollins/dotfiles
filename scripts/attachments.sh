c19f98083131df61ff0d08f7681a96ba73bbd091
Ryan 'Goz' Collins <goz@gozgeek.com>
2024-03-21 06:07:40 -0400

    Git Sync Thu 21 Mar 2024 06:07:40 AM EDT on venkman

--- scripts/attachments.sh
+++ scripts/attachments.sh
@@ -16,8 +16,10 @@ file="${1}"
 y=$(date +"%Y")
 #url="https://f000.backblazeb2.com/file/ryancollins-org/attachments/${y}"
 #url["rc"]="https://f000.backblazeb2.com/file/ryancollins-org/attachments/${y}"
-url["rc"]="https://cdn.collinsoft.com/file/cdn-collinsoft/ryancollins-org/${y}"
-url["e"]="https://cdn.collinsoft.com/file/cdn-collinsoft/eduk8.me/${y}"
+#url["rc"]="https://cdn.collinsoft.com/file/cdn-collinsoft/ryancollins-org/${y}"
+url["rc"]="https://dl.ryancollins.org/${y}"
+#url["e"]="https://cdn.collinsoft.com/file/cdn-collinsoft/eduk8.me/${y}"
+url["e"]="https://files.eduk8.me/${y}"
 
 #echo "URL "${url["${dest}"]}
 
@@ -25,7 +27,7 @@ shortname=$(basename "${file%.*}")
 slug=$(echo "${shortname}" | tr '[:upper:]' '[:lower:]' | sed -e 's/ /-/g' -e 's/[^a-z0-9-]//g')
 name="${d}-${slug}"
 ext="${file##*.}"
-out="/tmp/${name}"
+out="/tmp/attachments/${name}"
 mkdir -p "${out}"
 
 cp "${file}" "${out}/${name}.$ext"
@@ -34,10 +36,14 @@ ${convert} "${file}" -resize 1024x768 "${out}/${slug}-1024x.${ext}"
 ${convert} "${file}" -resize 2048x1536 "${out}/${slug}-2048x.${ext}"
 
 #remote["rc"]="rcorg:ryancollins-org/attachments/${y}/${name}"
-remote["rc"]="b2gg:cdn-collinsoft/ryancollins-org/${y}/${name}"
-remote["e"]="b2gg:cdn-collinsoft/eduk8.me/${y}/${name}"
+remote["rc"]="b2gg:cdn-collinsoft/ryancollins-org/${y}"
+remote["e"]="b2gg:cdn-collinsoft/eduk8.me/${y}"
 
 #echo "Remote "${remote["${dest}"]}
 
-${rclone} sync "${out}" ${remote["${dest}"]}
-echo ${url["${dest}"]}"/${name}/${slug}-1024x.${ext}"
+${rclone} copy "${out}" ${remote["${dest}"]}
+echo ${url["${dest}"]}"/${slug}-1024x.${ext}"
+
+# Clean up!
+rm -R "${out}"
+
