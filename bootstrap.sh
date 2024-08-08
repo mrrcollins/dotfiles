bba33cae0abfb59e60c0629b60708f98338c882e
Ryan 'Goz' Collins <goz@gozgeek.com>
2024-08-08 13:38:19 -0400

    Some changes to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -115,15 +115,16 @@ if [[ "${ostype}" =~ "Linux"]]; then
     if [ "${i}" == "y" ]; then
         if [ ! -d ~/.config/espanso ]; then
             git clone --quiet git@github.com:mrrcollins/espanso.git ~/.config/espanso
+
+            cd "/tmp"
+            wget https://github.com/federico-terzi/espanso/releases/latest/download/espanso-debian-${XDG_SESSION_TYPE}-amd64.deb
+            sudo apt install ./espanso-debian-${XDG_SESSION_TYPE}-amd64.deb
+            espanso service register
+            espanso start
         else
             echo "~/.config/espanso already exists..."
         fi
         
-        cd "/tmp"
-        wget https://github.com/federico-terzi/espanso/releases/latest/download/espanso-debian-${XDG_SESSION_TYPE}-amd64.deb
-        sudo apt install ./espanso-debian-${XDG_SESSION_TYPE}-amd64.deb
-        espanso service register
-        espanso start
     fi
 fi
 
