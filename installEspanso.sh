8fd6042d2f0291e5140f852affe967383b5d564d
Ryan 'Goz' Collins <goz@gozgeek.com>
2024-09-02 13:52:29 -0400

    Git Sync Mon Sep  2 01:52:29 PM EDT 2024 on goz-ohpmbp

--- installEspanso.sh
+++ installEspanso.sh
@@ -9,8 +9,18 @@ if [ "${i}" == "y" ]; then
     fi
     
     cd "/tmp"
-    wget https://github.com/federico-terzi/espanso/releases/latest/download/espanso-debian-${XDG_SESSION_TYPE}-amd64.deb
-    sudo apt install ./espanso-debian-${XDG_SESSION_TYPE}-amd64.deb
+
+    ##wget https://github.com/federico-terzi/espanso/releases/latest/download/espanso-debian-${XDG_SESSION_TYPE}-amd64.deb
+    ##sudo apt install ./espanso-debian-${XDG_SESSION_TYPE}-amd64.deb
+
+    # Create the $HOME/opt destination folder
+    mkdir -p ~/opt
+    # Download the AppImage inside it
+    wget -O ~/opt/Espanso.AppImage 'https://github.com/espanso/espanso/releases/download/v2.2.1/Espanso-X11.AppImage'
+    # Make it executable
+    chmod u+x ~/opt/Espanso.AppImage
+    # Create the "espanso" command alias
+    sudo ~/opt/Espanso.AppImage env-path register
     espanso service register
     espanso start
 fi
