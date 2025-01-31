d4469d8ae373e805f660df98eeb42ac673ecf96f
DESKTOP-HC53KP <DESKTOP-HC53KP@collinsoft.com>
2025-01-31 11:35:17 -0500

    Fixed path for dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -106,7 +106,9 @@ git config --global user.name "${gitname}"
 git config --global user.email "${gitemail}"
 
 echo "Set up dotfiles..."
+cd setup
 . setupdotfiles.sh
+..
 
 read -p "Install Nix? " i
 if [ "$i" == "y" ]; then
