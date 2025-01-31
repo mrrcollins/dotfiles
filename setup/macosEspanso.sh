543103423cf46219882423d755d304f073773035
DESKTOP-HC53KP <DESKTOP-HC53KP@collinsoft.com>
2025-01-31 11:32:29 -0500

    Installation of Espanso has changed for the Mac

--- setup/macosEspanso.sh
+++ setup/macosEspanso.sh
@@ -5,38 +5,47 @@ if [ ! -f /usr/local/espanso/bin/espanso ]; then
     cd ~/Downloads
 
     # Get it
-    curl -sOL https://github.com/federico-terzi/espanso/releases/latest/download/espanso-mac.tar.gz
+    machine=$(uname -a)
+    if [[ $machine =~ "ARM64" ]]; then
+        app="Espanso-Mac-M1.zip"
+    else
+        app="Espanso-Mac-Intel.zip"
+    fi
+    
+    curl -sOL https://github.com/federico-terzi/espanso/releases/latest/download/${app}
 
     # Decompress it
-    tar zxvf espanso-mac.tar.gz
+    unzip ${app}
 
     # Put it in place
-    sudo mkdir -p /usr/local/espanso/bin
-    sudo cp espanso /usr/local/espanso/bin/espanso
+    #sudo mkdir -p /usr/local/espanso/bin
+    #sudo cp espanso /usr/local/espanso/bin/espanso
+    mv Espanso.app ${HOME}/Applications
+    open -a ${HOME}/Applications/Espanso.app
 
     # Link it
-    sudo ln -s /usr/local/espanso/bin/espanso /usr/local/bin
+    #sudo ln -s /usr/local/espanso/bin/espanso /usr/local/bin/espanso
 
     # Get modulo
-    curl -sOL https://github.com/federico-terzi/modulo/releases/latest/download/modulo-mac
-    sudo cp modulo-mac /usr/local/bin/modulo
+    #curl -sOL https://github.com/federico-terzi/modulo/releases/latest/download/modulo-mac
+    #sudo cp modulo-mac /usr/local/bin/modulo
 
     # Check it
-    espanso --version
-    echo "Press a key to continue...";read -n 1 -s
+    #espanso --version
+    #echo "Press a key to continue...";read -n 1 -s
 
     # Get expansions
     cd ~/.config/
 
-    [[ -d espanso ]] || git clone https://github.com/mrrcollins/espanso.git
+    [[ -d espanso ]] || git clone git@github.com:mrrcollins/espanso.git
 
     cd espanso
     git pull
 
     # Register
-    espanso register
-    echo "Press a key to continue...";read -n 1 -s
+    #espanso register
+    #echo "Press a key to continue...";read -n 1 -s
 
-    espanso start
+    #espanso start
 fi
 
